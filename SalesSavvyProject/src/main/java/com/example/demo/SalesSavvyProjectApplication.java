aad3fa92f1e4b7779665fe22f0f5a721b43471ca
bhavani-reddy-kaipu <kaipubhavani@gmail.com>
2025-02-22 14:23:57 +0530

    first commit

---
+++ SalesSavvyProject/src/main/java/com/example/demo/SalesSavvyProjectApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+public class SalesSavvyProjectApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(SalesSavvyProjectApplication.class, args);
+		}
+}
+
+
